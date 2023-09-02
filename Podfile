6758138ab300f660e9bf87556e4c8ac289a312de
Hemang Solanki <hemang@thinkwik.com>
2023-09-02 12:16:42 +0530

    SAFE COMMIT

--- Podfile
+++ Podfile
@@ -4,17 +4,28 @@
 target 'Ecommerce' do
   # Comment the next line if you don't want to use dynamic frameworks
   use_frameworks!
-
+  
   # Pods for Ecommerce
   pod 'IQKeyboardManagerSwift'
-
+  pod 'SDWebImage'
+  pod 'SkeletonView'
+  pod 'SVProgressHUD'
   target 'EcommerceTests' do
     inherit! :search_paths
     # Pods for testing
   end
-
+  
   target 'EcommerceUITests' do
     # Pods for testing
   end
-
+  
+end
+post_install do |installer|
+  installer.generated_projects.each do |project|
+    project.targets.each do |target|
+      target.build_configurations.each do |config|
+        config.build_settings['IPHONEOS_DEPLOYMENT_TARGET'] = '13.0'
+      end
+    end
+  end
 end
--- Pods/Target Support Files/Pods-Ecommerce/Pods-Ecommerce-frameworks.sh
+++ Pods/Target Support Files/Pods-Ecommerce/Pods-Ecommerce-frameworks.sh
@@ -177,9 +177,15 @@ code_sign_if_enabled() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/IQKeyboardManagerSwift/IQKeyboardManagerSwift.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/SDWebImage/SDWebImage.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/SVProgressHUD/SVProgressHUD.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/SkeletonView/SkeletonView.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/IQKeyboardManagerSwift/IQKeyboardManagerSwift.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/SDWebImage/SDWebImage.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/SVProgressHUD/SVProgressHUD.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/SkeletonView/SkeletonView.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
